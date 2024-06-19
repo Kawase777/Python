213844db3c106060e7c74305287fc4ec098dc225
Kawase777 <95726563+Kawase777@users.noreply.github.com>
2024-06-19 11:06:14 -0300

    aula3

---
+++ aula3.py
@@ -0,0 +1,22 @@
+"""
+DocString
+Python = Linguagem de programação
+Tipo de tipagem = Dinâmica / Forte
+str -> string -> texto
+Strings são textos que estão dentro de aspas
+"""
+print(1234)
+
+# Aspas simples
+print('Gustavo Kawase')
+print(1, 'Gustavo "Kawase"')
+
+# Aspas duplas
+print("Gustavo Kawase")
+print(2, "Gustavo 'Kawase'")
+
+# Escape
+print("Gustavo \"Kawase\"")
+
+# r
+print(r"Gustavo \"Kawase\"")
