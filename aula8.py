694fc2b0a3937f1e8c94a0a1316a2c3d8f8d76f8
Kawase777 <95726563+Kawase777@users.noreply.github.com>
2024-07-09 14:26:57 -0300

    aula8_exercicio

---
+++ aula8.py
@@ -0,0 +1,17 @@
+nome = 'Gustavo'
+sobrenome = 'Kawase'
+idade = 35
+ano_de_nascimento = 1989
+maior_de_idade = idade >= 18
+altura_em_metros = 1.69
+
+
+
+print('Nome:', nome)
+print('Sobrenome:', sobrenome)
+print('Idade:', idade)
+print('Ano de Nascimento:', ano_de_nascimento)
+print('É Maior de Idade;', maior_de_idade)
+print('Altura em Metros;', altura_em_metros)
+
+
