4444580cde356c3dcdf2b472aa48b52df94cd486
Kawase777 <95726563+Kawase777@users.noreply.github.com>
2024-08-22 12:28:05 -0300

    aula17

---
+++ aula17.py
@@ -0,0 +1,19 @@
+# if / elif      / else
+# se / se não se / se não
+# Tab representa 4 espaços essa hierarquia representa 
+# que este trecho de codigo está dentro de if o trecho 
+# "print('você entrou no sistema)'''
+
+entrada = input('Vovê quer "entrar" ou "sair"? ')
+
+if entrada == 'entrar':
+    print('Você entrou no sistema')
+ 
+
+elif entrada == 'sair':
+    print('Você saiu do sistema')
+
+else:
+    print('Você não digitou nem entrar e nem sair.')
+
+
