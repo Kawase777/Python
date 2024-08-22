837e78d115e5bc4ce9da19b9e8aae104f969c27a
Kawase777 <95726563+Kawase777@users.noreply.github.com>
2024-08-22 12:43:13 -0300

    aula18

---
+++ aula18.py
@@ -0,0 +1,30 @@
+# if / elif      / else
+# se / se não se / se não
+# Tab representa 4 espaços essa hierarquia representa 
+# que este trecho de codigo está dentro de if o trecho 
+# "print('você entrou no sistema)'''
+
+# if / elif      / else
+# se / se não se / se não
+
+condicao1 = True
+condicao2 = True
+condicao3 = True
+condicao4 = True
+
+if condicao1:
+    print('Código para condição 1')
+    print('Código para condição 1')
+elif condicao2:
+    print('Código para condição 2')
+elif condicao3:
+    print('Código para condição 3')
+elif condicao4:
+    print('Código para condição 4')
+else:
+    print('Nenhuma condição foi satisfeita.')
+
+if 10 == 10:
+    print('Outro if')
+
+print('Fora do if')
